b224770c0f42408e7b6897519448b7c4e4b0497b
Jan Gorol <os.jangorol@gmail.com>
2022-10-29 18:17:47 +0200

    common module

--- src/app/containers/movies-list/movies-list.module.ts
+++ src/app/containers/movies-list/movies-list.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../../modules/material.module'
 import { MoviesListRoutingModule } from './movies-list-routing.module'
 import { ListComponent } from './/list/list.component';
@@ -10,6 +11,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent,
   ],
   imports: [
+    CommonModule,
     MoviesListRoutingModule,
     MaterialModule,
   ],
--- src/app/containers/movies-list/sidebar/sidebar.component.ts
+++ src/app/containers/movies-list/sidebar/sidebar.component.ts
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.less']
 })
 export class SidebarComponent implements OnInit {
+  genres: { name: string }[] = [
+    {name: 'Action'},
+    {name: 'Comedy'},
+    {name: 'Drama'},
+    {name: 'Fantasy'},
+    {name: 'Horror'},
+    {name: 'Mystery'},
+    {name: 'Romance'},
+    {name: 'Thriller'},
+  ]
+
+  users: string[] = ['John', 'Maria', 'Stan'];
 
   constructor() { }
 
